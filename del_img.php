5d0723b3bb396a44328d5ccb3ca92dd62ddf1f3c
php8database <php8database@gmail.com>
2024-11-18 14:42:53 +0800

    完成刪除資料表紀錄及圖片檔案功能

--- del_img.php
+++ del_img.php
@@ -1,5 +1,11 @@
 <?php
-
-$imgName=$_GET['file'];
+include_once "function.php";
+$id=$_GET['id'];
+$row=find('imgs',$id);
+$imgName=$row['filename'];
 unlink("./files/$imgName") ;
+del("imgs",$id);
+//$imgName=find('imgs',$id)['filename'];
+
+
 header("location:manage.php");
