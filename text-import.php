c26ece200c525a447e6e657dcc5b6e0c7549115f
php8database <php8database@gmail.com>
2025-02-21 11:11:06 +0800

    匯入CSV文字檔,並以html 表格方式以頁面呈現內容

--- text-import.php
+++ text-import.php
@@ -1,3 +1,22 @@
+<style>
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    border: 1px solid #ccc;
+    margin: 10px 0;
+}
+
+table th,
+table td {
+    border: 1px solid #ccc;
+    padding: 5px;
+}
+
+.header {
+    text-align: center;
+}
+</style>
+
 <?php
 /****
  * 1.建立資料庫及資料表
@@ -9,9 +28,40 @@
  * 7.結束檔案
  */
 
+if(!empty($_FILES['file'])){
+    move_uploaded_file($_FILES['file']['tmp_name'], "./files/{$_FILES['file']['name']}");
+    echo $_FILES['file']['name']."上傳成功";
+    getfile("./files/{$_FILES['file']['name']}");
+}
+
+
+function getfile($path){
+    $file=fopen($path,'r');
+    $line=fgets($file);
+    $cols=explode(",",trim($line));
+    echo "<table>";
+    echo "<tr class='header'>";
+    foreach($cols as $col){
+        echo "<th>{$col}</th>";
+    }
+    echo "</tr>";
+
+    while($line=fgets($file)){
+        $cols=explode(",",trim($line));
+        echo "<tr>";
+        foreach($cols as $col){
+            echo "<td>{$col}</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,15 +69,23 @@
     <title>文字檔案匯入</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<h1 class="header">文字檔案匯入練習</h1>
-<!---建立檔案上傳機制--->
+    <h1 class="header">文字檔案匯入練習</h1>
+    <!---建立檔案上傳機制--->
+    <form action="?" method="post" enctype="multipart/form-data">
+        <label for="file">文字檔:</label><input type="file" name="file" id="file">
+        <input type="submit" value="上傳">
+    </form>
 
 
+    <!----讀出匯入完成的資料----->
+    <?php 
 
-<!----讀出匯入完成的資料----->
 
+?>
 
 
 </body>
+
 </html>
