27c333698b07d4244aaa33a0afa0019c92cba07d
php8database <php8database@gmail.com>
2025-02-24 09:34:30 +0800

    text-import (文字檔案匯入)

--- PHP的檔案處理/del_img.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$imgName=$_GET['file'];
-unlink("./files/$imgName") ;
-header("location:manage.php");
--- PHP的檔案處理/file (FileZilla版)/conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DBNAME","s1130207");
-define("USERNAME","s1130207");
-define("USERPASSWORD","s1130207");
-
